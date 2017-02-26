2191109ec5d3e47655a2f2e94b3b3d77e0698bfb
Rameş Aliyev <creator@ramesaliyev.com>
2017-02-26 21:33:48 +0300

    Add new section and add notes about funcs etc.

--- code_samples/helloworld/main.go
+++ code_samples/helloworld/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("%#x \n", 1421)
+    fmt.Println(biggest(10, 20, 1, 3, 5, 99))
+}
+
+func biggest(nums ...int) int {
+    var most int = 0;
+
+    for _, i := range nums {
+        if i > most {
+            most = i
+        }
+    }
+
+    return most
 }
